60052ad9febc2e9da9e124d9b5adf43ab831cd7c
Sanjay <sanjayrjs16@gmail.com>
2020-12-12 00:55:16 +0530

    Solved sorted integers

--- LeetCode/Python/Largest Number.py
+++ LeetCode/Python/Largest Number.py
@@ -6,22 +6,54 @@
 # # print(str(arr[1])[0] <str(result_arr[0])[0])
 # print(int(math.log10(arr[0])) + 1)
 
-nums = [1, 1, 2, 2, 2, 2, 3,4, 4, 4,5, 5,6, 6,7]
-refElement = nums[0];
 
-for i in range(1, len(nums)):
-    if(refElement == nums[i] and i<len(nums) -1):
-        i += 1
-        print("Before Delete", nums)
-        while(nums[i]==refElement and i<len(nums)):
-            del nums[i]
-            i += 1
-            print("After delete", nums, nums[i])
-        refElement = nums[i]
-            
-    else:
-        refElement = nums[i+1]
+# Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
 
+# Do not allocate extra space for another array; you must do this by modifying the input array in-place with O(1) extra memory.
 
+# Clarification:
 
-print(nums)
+# Confused why the returned value is an integer, but your answer is an array?
+
+# Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller.
+
+# Internally you can think of this:
+
+# // nums is passed in by reference. (i.e., without making a copy)
+# int len = removeDuplicates(nums);
+
+# // any modification to nums in your function would be known by the caller.
+# // using the length returned by your function, it prints the first len elements.
+# for (int i = 0; i < len; i++) {
+#     print(nums[i]);
+# }
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if(len(nums) > 0):
+            refElement = nums[0]
+            i = 1
+            while(i<len(nums)-1):
+                if(refElement == nums[i]):
+                    i += 1
+                    
+                    while(nums[i]==refElement):
+
+                        del nums[i]
+                        if(i>=len(nums)):
+                            break
+
+                    if(i<len(nums)):
+                        refElement = nums[i]
+                        i+=1
+                    else:
+                        break
+
+                else:
+
+                    refElement = nums[i]
+                    i+=1
+        return len(nums)
